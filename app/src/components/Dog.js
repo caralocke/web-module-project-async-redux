ed145216fd54d118b520c682e3bd1ebcd394dc71
Cara Locke <cara.m.locke@gmail.com>
2021-07-14 12:47:54 -0400

    Started creating a component for getting dogs.

---
+++ app/src/components/Dog.js
@@ -0,0 +1,12 @@
+const Dog = () => {
+    return (
+        <div className='dog'>
+            <div>
+                <h2>Here's your dog!</h2>
+            </div>
+            <button>Get new dog</button>
+        </div>
+    )
+}
+
+export default Dog
